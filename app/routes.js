f1e0570c74225091a0d37b48af736b8d79b71f28
Yowks <48799889+Yowks@users.noreply.github.com>
2020-05-13 21:40:37 +0200

    :Routes Fix

--- app/routes.js
+++ app/routes.js
@@ -3,11 +3,13 @@ const Event = require('./controllers/event.js')
 const Group = require('./controllers/group.js')
 const Comment = require('./controllers/comment.js')
 const Message = require('./controllers/message.js')
+const Discussion = require('./controllers/discussion.js')
 
 module.exports = {
   User,
   Event,
   Group,
   Comment,
-  Message
+  Message,
+  Discussion
 }
